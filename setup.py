57f7babbca505e253796618ca969798c17fc3ea4
Collin Anderson <cmawebsite@gmail.com>
2014-12-30 17:20:33 -0500

    roll version 1.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme:
 
 setup(
     name='diy-django',
-    version='1.2.1',
+    version='1.3.0',
     packages=['diydjango'],
     install_requires=['django'],
     description='diy-django',
